c06600cc634ffec7362dcff2dba937b936ba2e4b
Emily Best <emilyabest@fb.com>
2019-07-03 11:57:11 -0700

    About to start bar button attempt. Navigation VC for compose is set up.

---
+++ twitter/View Controllers/ComposeViewController.m
@@ -0,0 +1,37 @@
+//
+//  ComposeViewController.m
+//  twitter
+//
+//  Created by emilyabest on 7/3/19.
+//  Copyright © 2019 Emerson Malca. All rights reserved.
+//
+
+#import "ComposeViewController.h"
+
+@interface ComposeViewController ()
+@property (weak, nonatomic) IBOutlet UITextView *composeSpace;
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *closeButton;
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *tweetButton;
+
+@end
+
+@implementation ComposeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    [self.closeButton action[self dismissViewControllerAnimated:true completion:nil]];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
